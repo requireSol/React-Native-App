fe0f174bb9fec8760d6f637802b5417b72201a5e
requireSol <enamsolaimani@icloud.com>
2019-04-07 06:57:51 +0200

    Add Auth Flow Try to fix White Border

--- App.js
+++ App.js
@@ -1,90 +1,168 @@
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  StatusBar,
-  Button
+    ActivityIndicator,
+    Button,
+    StatusBar,
+    StyleSheet,
+    View,
+    Platform
 } from 'react-native';
-import { createStackNavigator, createAppContainer  , StackActions,
-    NavigationActions} from "react-navigation";
-
-
+//import { Button } from 'react-native-elements';
+import AsyncStorage from '@react-native-community/async-storage';
+import {createStackNavigator, createSwitchNavigator, createAppContainer} from 'react-navigation';
 import SplashScreen from 'react-native-splash-screen';
 
-
-
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-      'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-      'Shake or press menu button for dev menu',
+    ios: 'Press Cmd+R to reload,\n' +
+        'Cmd+D or shake for dev menu',
+    android: 'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
 });
-class HomeScreen extends Component {
+
+class SignInScreen extends React.Component {
     componentDidMount() {
         SplashScreen.hide()
     }
-  render() {
-      return (
-          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-              <Text>Home Screen</Text>
-              <Button
-                  title="Go to Details"
-                  onPress={() => {
-                      this.props.navigation.dispatch(StackActions.reset({
-                          index: 0,
-                          actions: [
-                              NavigationActions.navigate({ routeName: 'Details' })
-                          ],
-                      }))
-                  }}
-              />
-          </View>
-      );
-  }
-}
 
-class DetailsScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Please sign in',
+        headerStyle: {
+            backgroundColor: '#2A001A',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            fontWeight: 'bold',
+        },
+        shadowColor: '#fff',
+    };
+
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Details Screen</Text>
+            <View style={styles.container}>
+                <Button title="Sign in!" onPress={this._signInAsync}/>
+            </View>
+        );
+    }
+
+    _signInAsync = async () => {
+        await AsyncStorage.setItem('userToken', 'abc');
+        this.props.navigation.navigate('App');
+    };
+}
+
+class HomeScreen extends React.Component {
+    componentDidMount() {
+        SplashScreen.hide()
+    }
+    static navigationOptions = {
+        title: 'Welcome to the app!',
+            headerStyle: {
+                backgroundColor: '#2A001A',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+            shadowColor: '#fff',
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button title="Show me more of the app" />
+                <Button
+                    title="Outline button"
+                    type="outline"
+                    onPress={this._showMoreApp}
+                />
+                <Button title="Actually, sign me out :)" onPress={this._signOutAsync}/>
+            </View>
+        );
+    }
+
+    _showMoreApp = () => {
+        this.props.navigation.navigate('Other');
+    };
+
+    _signOutAsync = async () => {
+        await AsyncStorage.clear();
+        this.props.navigation.navigate('Auth');
+    };
+}
+
+class OtherScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Lots of features here',
+        headerStyle: {
+            backgroundColor: '#2A001A',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            fontWeight: 'bold',
+        },
+        shadowColor: '#fff',
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Button title="I'm done, sign me out" onPress={this._signOutAsync}/>
+                <StatusBar barStyle="default"/>
+            </View>
+        );
+    }
+
+    _signOutAsync = async () => {
+        await AsyncStorage.clear();
+        this.props.navigation.navigate('Auth');
+    };
+}
+
+class AuthLoadingScreen extends React.Component {
+    constructor() {
+        super();
+        this._bootstrapAsync();
+    }
+
+    // Fetch the token from storage then navigate to our appropriate place
+    _bootstrapAsync = async () => {
+        const userToken = await AsyncStorage.getItem('userToken');
+
+        // This will switch to the App screen or Auth screen and this loading
+        // screen will be unmounted and thrown away.
+        this.props.navigation.navigate(userToken ? 'App' : 'Auth');
+    };
+
+    // Render any loading content that you like here
+    render() {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator/>
+                <StatusBar barStyle="default"/>
             </View>
         );
     }
 }
 
-const AppNavigator = createStackNavigator({
-    Home: {
-        screen: HomeScreen,
-    },
-    Details: {
-        screen: DetailsScreen,
-    },
-}, {
-    initialRouteName: 'Home',
-});
-
-export default createAppContainer(AppNavigator);
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#2A001A',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: '#F5FCFF',
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#F5FCFF',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#2A001A'
+    },
 });
+
+const AppStack = createStackNavigator({Home: HomeScreen, Other: OtherScreen});
+const AuthStack = createStackNavigator({SignIn: SignInScreen});
+
+export default createAppContainer(createSwitchNavigator(
+    {
+        AuthLoading: AuthLoadingScreen,
+        App: AppStack,
+        Auth: AuthStack,
+    },
+    {
+        initialRouteName: 'AuthLoading',
+    }
+));
